8e98174a08a0a507394dc45eb12c006ba5101ecf
katie19kl <59045304+katie19kl@users.noreply.github.com>
2020-03-21 10:39:35 +0200

    bla

---
+++ FlightSimulatorApp/Joystick.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace FlightSimulatorApp
+{
+    public partial class Joystick: UserControl
+    {
+
+        private bool isPressed = false;
+
+        public Joystick()
+        {
+            InitializeComponent();
+        }
+
+        void centerKnob_Completed(object sender, EventArgs e)
+        {
+            //empty
+        }
+
+        private void RealKnob_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            isPressed = true;
+           
+        }
+
+        private void RealKnob_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            isPressed = false;
+           
+
+             
+
+            /*Canvas.SetLeft(this.RealKnob, 0);
+            Canvas.SetTop(this.RealKnob, 0);
+
+            Canvas.SetLeft(this.KnobBase, 0);
+            Canvas.SetTop(this.KnobBase, 0);
+
+            Canvas.SetLeft(this.LeftEllipse, 0);
+            Canvas.SetTop(this.LeftEllipse, 0);
+
+            Canvas.SetLeft(this.MiddleRightEllipse, 0);
+            Canvas.SetTop(this.MiddleRightEllipse, 0);
+
+            Canvas.SetLeft(this.LeftEllipse2, 10);
+            Canvas.SetTop(this.LeftEllipse2, 10);
+
+            Canvas.SetLeft(this.SmallLeftEllipse, 10);
+            Canvas.SetTop(this.SmallLeftEllipse, 10);
+            
+            Canvas.SetLeft(this.whiteThing, 16);
+            Canvas.SetTop(this.whiteThing, 16);
+            */
+        }
+
+        private void RealKnob_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (isPressed)
+            {
+                Point position = e.GetPosition(this.centerEllipse);
+
+                knobPosition.X = position.X;
+                knobPosition.Y = position.Y;
+
+
+                /*Canvas.SetLeft(this.RealKnob, position.X);
+                Canvas.SetTop(this.RealKnob, position.Y);
+
+
+                Canvas.SetLeft(this.KnobBase, position.X);
+                Canvas.SetTop(this.KnobBase, position.Y);
+
+                Canvas.SetLeft(this.LeftEllipse, position.X);
+                Canvas.SetTop(this.LeftEllipse, position.Y);
+
+                Canvas.SetLeft(this.MiddleRightEllipse, position.X);
+                Canvas.SetTop(this.MiddleRightEllipse, position.Y);
+
+                Canvas.SetLeft(this.LeftEllipse2, position.X + 10);
+                Canvas.SetTop(this.LeftEllipse2, position.Y + 10);
+
+                Canvas.SetLeft(this.SmallLeftEllipse, position.X + 10);
+                Canvas.SetTop(this.SmallLeftEllipse, position.Y + 10);
+
+                Canvas.SetLeft(this.whiteThing, position.X + 16);
+                Canvas.SetTop(this.whiteThing, position.Y + 16);*/
+
+
+                
+            }
+        }
+    }
+}
---
+++ FlightSimulatorApp/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightSimulatorApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Joystick_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Joystick_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Joystick_Loaded_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Joystick_Loaded_3(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double val = Convert.ToDouble(e.NewValue);
+            string msg = String.Format("current value is: {0}", val);
+            t1.Text = msg;
+        }
+
+        private void s2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double val = Convert.ToDouble(e.NewValue);
+            string msg = String.Format("current value is: {0}", val);
+            t1_Copy.Text = msg;
+        }
+    }
+}
